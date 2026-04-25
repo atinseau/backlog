d5d2e8c5db78c6ec0ba375ec18bcb12085556b8a
jimdou <jimmydouieb@gmail.com>
2026-04-25 14:17:08 +0200

    Add backlog auth login/logout/status/whoami/signup commands wiring backlog-server

--- packages/cli/src/bin.ts
+++ packages/cli/src/bin.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import { Command } from "commander";
 import { registerAgentCommand } from "./commands/agent.js";
+import { registerAuthCommand } from "./commands/auth.js";
 import { registerClaimCommand } from "./commands/claim.js";
 import { registerDoctorCommand } from "./commands/doctor.js";
 import { registerHooksCommand } from "./commands/hooks.js";
@@ -29,6 +30,7 @@ program
 
 registerInitCommand(program);
 registerDoctorCommand(program);
+registerAuthCommand(program);
 registerAgentCommand(program);
 registerClaimCommand(program);
 registerHooksCommand(program);
